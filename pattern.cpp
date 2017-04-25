b7311dd6fbe79155e9c25971b8f31c9b0e2bb23e
msamatar0 <mohamedsamatar100@gmail.com>
2017-04-24 17:26:03 -0700

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -5,16 +5,17 @@
  */
 #include <iostream>
 #include <string>
+#include <vector>
 #include <functional>
 using namespace std;
 
-//Returned by search-type functions
+//Returned by search -type functions
 //indicates found index and # of comparisons used
 struct search{
 	int idx, comparisons;
 };
 
-void searchFunc(function<search(string, string)> func, string text, string pattern);
+string searchFunc(function<search(string, string)> func, string text, string pattern);
 search bruteForceSearch(string t, string p);
 search bmMatch(string t, string p);
 search kmpMatch(string t, string p);
@@ -22,21 +23,20 @@ search kmpMatch(string t, string p);
 int main(){
 	cout << "/*\n * Mohamed Samatar - 101848\n * Assignment 13 - Pattern Matching\n"
 		<< " * Description - Compares the 3 pattern-matching algorithms\n */\n\n";
+
 	string text = "aaabcaadaabaaa", pattern = "aabaaa";
-	cout << "P1 - Brute Force:\n";
-	searchFunc(bruteForceSearch, text, pattern);
-	cout << "P2 - Boyer-Moore:\n";
-	searchFunc(bmMatch, text, pattern);
-	cout << "P3 - KMP:\n";
-	searchFunc(kmpMatch, text, pattern);
+	cout << "P1 - Brute Force:\n" << searchFunc(bruteForceSearch, text, pattern);
+	cout << "P2 - Boyer-Moore:\n" << searchFunc(bmMatch, text, pattern);
+	cout << "P3 - KMP:\n" << searchFunc(kmpMatch, text, pattern);
 }
 
-//Calls search function and outputs results
-void searchFunc(function<search(string, string)> func, string text, string pattern){
+//Takes search function and returns string based on results
+string searchFunc(function<search(string, string)> func, string text, string pattern){
 	search result = func(text, pattern);
-	cout << text << (result.idx != -1 ? " contains" : " does not contain")
-		<< " " << pattern << " starting at index #" << result.idx << ": "
-		<< result.comparisons << " comparisons used\n\n";
+	bool found = result.idx != -1;
+	return text + (found? " contains" : " does not contain") + " "
+		+ pattern + (found? " starting at index #" + to_string(result.idx) : "")
+		+ ": " + to_string(result.comparisons) + " comparisons used\n\n";
 }
 
 //Brute Force Matching algorithm
@@ -96,19 +96,20 @@ search kmpMatch(string t, string p){
 			fail[i++] = 0;
 	}
 	while(i < n){
+		comps++;
 		if(t[i] == p[j]){
 			if(j == m - 1){
 				delete[] fail;
-				return{i - m + 1, ++comps};
+				return{ i - m + 1, comps };
 			}
 			i++;
 			j++;
+			comps++;
 		}
 		else if(j > 0)
 			j = fail[j - 1];
 		else
 			i++;
-		comps++;
 	}
 	delete[] fail;
 	return{ -1, comps };
